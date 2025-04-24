42a8264f29cbd29b3767fdce6e6b810eda05f402
gimeneznacho07 <136719918+gimeneznacho07@users.noreply.github.com>
2025-04-23 22:49:37 -0300

    Add files via upload

--- EJ 2 TP4.cpp
+++ EJ 2 TP4.cpp
@@ -4,20 +4,45 @@ FILE *archivo;
 char nombre[20], apellido[20], nombreBusq[20], apellidoBusq[20]; 
 int dni, opc, dni_busq;
 main(){
-	printf("Ingrese la opcion:\n 1. Guardar datos en un archivo\n 2. Buscar por dni\n 3. Salir\n 4. Buscar por nombre\n 5. Buscar por apellido\n");
+	printf("Ingrese la opcion:\n 1. Guardar datos en un archivo\n 2. Buscar por dni\n 3. Salir\n 4. Buscar por nombre y apellido\n");
 	scanf("%d", &opc);
 	switch(opc){
-		case 1:
+		case 1:{
+			int nombreRep=0, apellidoRep=0, dniRep=0;
 			printf("Ingrese su nombre: \n");
 			scanf("%s", nombre);
 			printf("Ingrese su apellido: \n");
 			scanf("%s", apellido);
 			printf("Ingrese su DNI: \n");
 			scanf("%d", &dni); 
-			archivo = fopen("guardar.txt", "a");
-			fprintf(archivo, "%s %s %d\n", nombre, apellido, dni);
-			fclose(archivo);
+			archivo = fopen("guardar.txt", "r");
+			while(fscanf(archivo, "%s %s %d", nombreBusq, apellidoBusq, &dni_busq) != EOF) 
+			{
+                 if(strcmp(nombreBusq, nombre) == 0) 
+				{
+                   nombreRep++;
+                }
+                 if(strcmp(apellidoBusq, apellido) == 0)
+				{
+                   apellidoRep++;
+                }
+                if(dni_busq == dni) 
+				{
+                   dniRep++;
+                }
+           }
+            fclose(archivo);
+            if(nombreRep != 0 && apellidoRep != 0 && dniRep != 0)
+            {
+            	printf("Los datos ya estan registrados.");
+			}
+			else{
+				archivo = fopen("guardar.txt", "a");
+				fprintf(archivo, "%s %s %d\n", nombre, apellido, dni);
+				fclose(archivo);
+			}
 			break;
+		}
 		case 2:
 			printf("Ingrese el dni que quiere buscar: \n");
 			scanf("%d", &dni_busq);
@@ -37,26 +62,13 @@ main(){
 		case 4:
 			printf("Ingrese el nombre que quiere buscar: \n");
 			scanf("%s", &nombreBusq);
-			archivo = fopen("guardar.txt", "r");
-			while(fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF)
-			{
-				if(strcmp(nombre, nombreBusq) == 0)
-				{
-				printf("Su dni es: %d\n", dni);
-				printf("Su apellido es: %s\n", apellido);
-				}
-			}
-			fclose(archivo);
-			break;
-		case 5:
 			printf("Ingrese el apellido que quiere buscar: \n");
 			scanf("%s", &apellidoBusq);
 			archivo = fopen("guardar.txt", "r");
 			while(fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF)
 			{
-				if(strcmp(apellido, apellidoBusq) == 0)
+				if(strcmp(nombre, nombreBusq) == 0 && strcmp(apellido, apellidoBusq) == 0)
 				{
-				printf("Su nombre es: %s\n", nombre);
 				printf("Su dni es: %d\n", dni);
 				}
 			}

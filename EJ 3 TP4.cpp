b72957c027cd01fe5f637e04b8465bf43406e9ad
gimeneznacho07 <136719918+gimeneznacho07@users.noreply.github.com>
2025-04-23 22:52:59 -0300

    Add files via upload

--- EJ 3 TP4.cpp
+++ EJ 3 TP4.cpp
@@ -4,7 +4,7 @@ FILE *archivo;
 char nombre[20], apellido[20], nombreBusq[20], apellidoBusq[20]; 
 int dni, opc, dni_busq;
 main(){
-	printf("Ingrese la opcion:\n 1. Guardar datos en un archivo\n 2. Buscar por dni\n 3. Salir\n 4. Buscar por nombre\n 5. Buscar por apellido\n");
+	printf("Ingrese la opcion:\n 1. Guardar datos en un archivo\n 2. Buscar por dni\n 3. Salir\n 4. Buscar por nombre y apellido\n");
 	scanf("%d", &opc);
 	switch(opc){
 		case 1:{
@@ -62,26 +62,13 @@ main(){
 		case 4:
 			printf("Ingrese el nombre que quiere buscar: \n");
 			scanf("%s", &nombreBusq);
-			archivo = fopen("guardar.txt", "r");
-			while(fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF)
-			{
-				if(strcmp(nombre, nombreBusq) == 0)
-				{
-				printf("Su dni es: %d\n", dni);
-				printf("Su apellido es: %s\n", apellido);
-				}
-			}
-			fclose(archivo);
-			break;
-		case 5:
 			printf("Ingrese el apellido que quiere buscar: \n");
 			scanf("%s", &apellidoBusq);
 			archivo = fopen("guardar.txt", "r");
 			while(fscanf(archivo, "%s %s %d", nombre, apellido, &dni) != EOF)
 			{
-				if(strcmp(apellido, apellidoBusq) == 0)
+				if(strcmp(nombre, nombreBusq) == 0 && strcmp(apellido, apellidoBusq) == 0)
 				{
-				printf("Su nombre es: %s\n", nombre);
 				printf("Su dni es: %d\n", dni);
 				}
 			}
